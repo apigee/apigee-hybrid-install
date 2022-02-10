d444f080794de40c26c4f66770e2ee93827d4b0b
Brijesh Panara <brijeshpanara@google.com>
2022-02-10 19:58:10 +0530

    Change image tag for components image
    GitOrigin-RevId: 553601f928bd3305b6c5ad2e98fb20965eed6842

--- tools/apigee-pull-push.sh
+++ tools/apigee-pull-push.sh
@@ -3,13 +3,14 @@
 # Exit immediately if sequence of one or more commands returns a non-zero status.
 set -eo pipefail
 
+SCRIPT_NAME=$(basename "${0}")
+SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
 # shellcheck disable=SC1090
-source "${PWD}/common.sh" || exit 1
+source "${SCRIPT_DIR}/common.sh" || exit 1
 
 HELP=0
-REPO="us.gcr.io"
-PROJECT_ID=""
-TAG="${APIGEE_HYBRID_VERSION}" 
+REPOSITORY=""
+TAG="${APIGEE_HYBRID_VERSION}"
 
 APIGEE_COMPONENTS=("apigee-mart-server" "apigee-synchronizer" "apigee-runtime" "apigee-hybrid-cassandra-client" "apigee-hybrid-cassandra" "apigee-cassandra-backup-utility" "apigee-udca" "apigee-connect-agent" "apigee-watcher" "apigee-operators" "apigee-installer" "apigee-redis" "apigee-diagnostics-collector" "apigee-diagnostics-runner")
 THIRD_PARTY_COMPONENTS=("apigee-stackdriver-logging-agent:1.8.9" "apigee-prom-prometheus:v2.25.0" "apigee-stackdriver-prometheus-sidecar:0.9.0" "apigee-kube-rbac-proxy:v0.8.0" "apigee-envoy:v1.16-latest" "apigee-prometheus-adapter:v0.9.1")
@@ -32,33 +33,40 @@ main() {
         exit 0
     fi
 
-    echo "$(basename $0) will push images to $REPO:$PROJECT_ID"
+    echo "${SCRIPT_NAME} will push images to ${REPOSITORY}"
 
     docker_exe "pull" "google" "${TAG}"
-    docker_tag "google" "${REPO}" "${TAG}"
-    docker_exe "push" "${REPO}" "${TAG}"
+    docker_tag "google" "${REPOSITORY}" "${TAG}"
+    docker_exe "push" "${REPOSITORY}" "${TAG}"
 }
 
 ################################################################################
 # Displays apigee pull push script usage details, when --help flag is specified.
 ################################################################################
 usage() {
-    echo "Usage: $(basename \"$0\") [repo where you want to push the images]" \
-        "Note: if the repo is not provided. It will be pushed to us.gcr.io/<PROJECT_ID>.\\n" \
-        "      Please make sure you have gcloud and docker installed.\\n\\n" \
-        "example: $(basename "$0") [foo.docker.com]"
+cat <<EOF
+USAGE: ${SCRIPT_NAME} --repo <REPOSITORY>
+
+FLAGS:
+
+  --repo        <REPOSITORY>    Repository where you want to push the images.
+
+  --project-id  <PROJECT_ID>    If the repository is not provided, Images will
+                                be pushed to "us.gcr.io/<PROJECT_ID>".
+
+NOTE: 1. Please make sure you have docker installed.
+      2. If <PROJECT_ID> is not provided, and you have gcloud installed
+         then it uses gcloud default configured <PROJECT_ID> to push images.
+
+Example: ${SCRIPT_NAME} --repo=foo.docker.com/hybrid-images
+         ${SCRIPT_NAME} --project-id=hybrid-setup
+EOF
 }
 
 ################################################################################
 # Validate prerequisite.
 ################################################################################
 validate_prerequisite() {
-    # Check gcloud is installed and on the $PATH.
-    if ! which gcloud > /dev/null 2>&1; then
-        echo "gcloud is not installed or not on PATH."
-        exit 0
-    fi
-
     # Check docker is installed and on the $PATH.
     if ! which docker > /dev/null 2>&1; then
         echo "docker is not installed or not on PATH."
@@ -67,14 +75,8 @@ validate_prerequisite() {
 }
 
 ################################################################################
-# Parse command line arguments.
+# Parse arguments.
 ################################################################################
-arg_required() {
-    if [[ ! "${2:-}" || "${2:0:1}" = '-' ]]; then
-        fatal "Option ${1} requires an argument."
-    fi
-}
-
 parse_args() {
     while [[ $# != 0 ]]; do
         case "${1}" in
@@ -84,12 +86,12 @@ parse_args() {
             ;;
         --repo)
             arg_required "${@}"
-            REPO="${2}"
+            REPOSITORY="${2}"
             shift 2
             ;;
-        --project-id)   
+        --project-id)
             arg_required "${@}"
-            PROJECT_ID="${2}"
+            REPOSITORY="us.gcr.io/${2}"
             shift 2
             ;;
         *)
@@ -103,13 +105,19 @@ parse_args() {
 # Tries to configure the default values of unset variables.
 ################################################################################
 configure_defaults() {
-    # Configure project id.
-    if [[ -z "${PROJECT_ID}" ]]; then
+    # If repository and project-id is not provided.
+    if [[ -z "${REPOSITORY}" ]]; then
+      local PROJECT_ID
+      # If gcloud is installed and on the $PATH.
+      if ! which gcloud > /dev/null 2>&1; then
         PROJECT_ID="$(gcloud config get-value project)"
+      fi
+      # If gcloud configured project-id is not empty.
+      if [[ -n "${PROJECT_ID}" ]]; then
+        REPOSITORY="us.gcr.io/${PROJECT_ID}"
+      fi
     fi
-    readonly PROJECT_ID
 }
-
 
 ################################################################################
 # Validate command line arguments.
@@ -119,13 +127,8 @@ validate_args() {
         return
     fi
 
-    if [[ -z "${REPO}" ]]; then
-        echo "Repository is empty, it can not be empty."
-        exit 0
-    fi
-
-    if [[ -z "${PROJECT_ID}" ]]; then
-        echo "Project id is empty, it can not be empty."
+    if [[ -z "${REPOSITORY}" ]]; then
+        log_error "Repository and project-id both are not provided. Please check usage: \"${SCRIPT_NAME} --help\""
         exit 0
     fi
 
@@ -145,12 +148,19 @@ docker_exe() {
 
     for i in "${APIGEE_COMPONENTS[@]}"
     do
-        docker "${action}" "${repo}/$i:${tag}"
+        docker "${action}" "${repo}/${i}:${tag}"
+        if [[ "${action}" == "push" ]]; then
+          docker "${action}" "${repo}/${i}:apigee-${tag}"
+        fi
     done
 
     for i in "${THIRD_PARTY_COMPONENTS[@]}"
     do
-        docker "${action}" "${repo}/$i"
+        docker "${action}" "${repo}/${i}"
+        if [[ "${action}" == "push" ]]; then
+          readarray -d : -t component <<< "${i}"
+          docker "${action}" "${repo}/${component[0]}:apigee-${tag}"
+        fi
     done
 }
 
@@ -161,12 +171,15 @@ docker_tag() {
 
     for i in "${APIGEE_COMPONENTS[@]}"
     do
-        docker tag "${source}/$i:${tag}" "${dest}/$i:${tag}"
+        docker tag "${source}/${i}:${tag}" "${dest}/${i}:${tag}"
+        docker tag "${source}/${i}:${tag}" "${dest}/${i}:apigee-${tag}"
     done
 
     for i in "${THIRD_PARTY_COMPONENTS[@]}"
     do
-        docker tag "${source}/$i" "${dest}/$i"
+        readarray -d : -t component <<< "${i}"
+        docker tag "${source}/${i}" "${dest}/${i}"
+        docker tag "${source}/${i}" "${dest}/${component[0]}:apigee-${tag}"
     done
 }
 
