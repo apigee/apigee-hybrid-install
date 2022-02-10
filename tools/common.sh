553601f928bd3305b6c5ad2e98fb20965eed6842
Brijesh Panara <brijeshpanara@google.com>
2022-02-10 19:58:10 +0530

    b/218863790 Change image tag for components image
    Change-Id: Ifac80d6cddbde39b9ef024eb7e30da8d11298ead

--- tools/common.sh
+++ tools/common.sh
@@ -2,11 +2,24 @@
 
 APIGEE_HYBRID_VERSION="1.8.0"   # Apigee hybrid version that will be installed.
 
+################################################################################
+# Functions for logging error and info.
+################################################################################
+
 log_error(){
-    printf "\\n[ERROR]: %b\\n" "$*"
+    printf "\\n[ERROR]: %b\\n\\n" "$*"
     exit 1
 }
 
 log_info(){
     printf "\\n[INFO]: %b\\n" "$*"
+}
+
+################################################################################
+# Checks for the existence of a second argument and exit if it does not exist.
+################################################################################
+arg_required() {
+    if [[ ! "${2:-}" || "${2:0:1}" = '-' ]]; then
+        fatal "Option ${1} requires an argument."
+    fi
 }
